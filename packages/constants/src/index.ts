67a3c3297d78cf2fe3ef88fdf8f9e59c018a6c22
Abdullahi Abdullateef Al-mujahid <almujahidalimam@gmail.com>
2026-09-26 10:26:18 +0100

    fix(constants): export wedding production conclusion helper

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -24,6 +24,7 @@ export {
   isUsableTermiiSenderId,
   isWeddingModalEligible,
   isWeddingProductionLive,
+  isWeddingProductionConcluded,
   isWeddingTestLive,
   normalizeWeddingSmsPhone,
   shouldRequestWeddingLiveToken,
